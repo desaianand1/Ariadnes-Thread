14aa93cb26095d63fbec2a0e74be6af0bf9dc34d
Anand Desai <desaianand456@gmail.com>
2024-12-08 02:05:37 +0530

    style: font change to Comfortaa, modified some colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,7 +67,7 @@ const config = {
 				sm: "calc(var(--radius) - 4px)"
 			},
 			fontFamily: {
-				sans: ['Oswald',...fontFamily.sans]
+				sans: ['Comfortaa',...fontFamily.sans]
 			},
 			keyframes: {
 				"accordion-down": {
